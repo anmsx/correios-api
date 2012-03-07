6ce031ffa6eb044bb270b8c8061230061e788ec9
Eziel Silva <silva.eziel@gmail.com>
2012-03-06 23:36:14 -0300

    Removidos outros itens que nao serao utilizados

--- lib/correios_rest_api.rb
+++
@@ -1,8 +0,0 @@
-require "rubygems"
-require "sinatra"
-require "correios"
-
-get "/encomenda/:numero" do
-  encomenda = Correios.encomenda(params[:numero])
-  xml = "<? xml encoding=\"UTF-8\" version=\"1.0\" ?>\n#{encomenda.to_xml}"
-end
